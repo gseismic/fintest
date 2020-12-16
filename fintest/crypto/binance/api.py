a6e0099c8630ad3ecce1e2f7f65d70d442e27396
Liu Shengli <liushengli203@163.com>
2020-12-16 08:33:45 +0800

    Modify: sqlite.Row

--- fintest/crypto/binance/api.py
+++ fintest/crypto/binance/api.py
@@ -1,6 +1,9 @@
 import arrow
 import sqlite3
-from fintest.crypto.binance.config import BN_BTCUSDT_2020_SQLITE3
+from fintest.crypto.binance.config import (
+    BN_BTCUSDT_2020_SQLITE3, 
+    BN_BTCUSDT_20201209_0_TRADES_SQLITE3 
+)
 from fintest.utils.io import ensure_xzfile_decompressed
 
 
@@ -21,16 +24,40 @@ def get_future_klines(symbol, interval, start_dt, end_dt):
     _start_dt = to_unixepoch(start_dt, to_ms=True)
     _end_dt = to_unixepoch(end_dt, to_ms=True)
     _symbol = symbol.upper()
+    # [@2020-12-15 16:25:14] fix
+    assert(symbol == 'BTCUSDT')
+    # if _symbol == 'BTCUSDT': _symbol = 'TBCUSDT'
     ensure_xzfile_decompressed(BN_BTCUSDT_2020_SQLITE3)
     conn = sqlite3.connect(BN_BTCUSDT_2020_SQLITE3)
+    conn.row_factory = sqlite3.Row
     try:
         cu = conn.cursor()
         try:
             query_sql = f'''SELECT * 
-            FROM BTCUSDT_1m where symbol="{_symbol}" and open_time between
+            FROM TBCUSDT_1m where open_time between
             {_start_dt} and {_end_dt}'''
             cu.execute(query_sql)
-            data = cu.fetchall()
+            data = [dict(r) for r in cu.fetchall()]
+        finally:
+            cu.close()
+    finally:
+        conn.close()
+    return data
+
+
+def load_():
+    dbfile = BN_BTCUSDT_20201209_0_TRADES_SQLITE3
+    ensure_xzfile_decompressed(dbfile)
+    conn = sqlite3.connect(dbfile)
+    conn.row_factory = sqlite3.Row
+    try:
+        cu = conn.cursor()
+        try:
+            query_sql = f'''SELECT * 
+            FROM TBCUSDT_1m where open_time between
+            {_start_dt} and {_end_dt}'''
+            cu.execute(query_sql)
+            data = [dict(r) for r in cu.fetchall()]
         finally:
             cu.close()
     finally:
--- tests/test_crypto/test_binance.py
+++ tests/test_crypto/test_binance.py
@@ -5,10 +5,11 @@ from fintest.crypto.binance import api
 
 def test_get_future_klines():
     data = api.get_future_klines(symbol='BTCUSDT', 
+                                 interval='1m',
                                  start_dt='2020-11-20', 
-                                 end_dt='2020-11-21')
+                                 end_dt='2020-11-25')
     print(data, len(data))
-    assert(len(data) == 12*2*60 + 1)
+    # assert(len(data) == 12*2*60 + 1)
 
 
 if __name__ == '__main__':
